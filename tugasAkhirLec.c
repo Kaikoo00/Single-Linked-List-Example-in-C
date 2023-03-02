714c8c56841a449af7b289d6b4cd6ea5150615c7
Kaikoo00 <116528497+Kaikoo00@users.noreply.github.com>
2023-03-02 17:15:23 +0700

    Update tugasAkhirLec.c

--- tugasAkhirLec.c
+++ tugasAkhirLec.c
@@ -31,13 +31,48 @@ void insert_tail(int value){
     tail->next=newnode;
     tail = tail->next;
 }
+
+void delete_head(){
+    head = head->next;
+}
+
+void delete_element(int pos){
+    if(pos>=data_count){
+        return;
+    }
+
+    if(pos==0){
+        delete_head();
+        return;
+    }
+
+    else{
+        node *curr = head;
+        node *prev;
+        node *next;
+        for(int i=0; i<pos;i++){
+            prev = curr;
+            curr = curr->next;
+            if(curr==tail){
+                free(curr);
+                tail = prev;
+                return;
+            }
+        }
+        prev->next = curr->next;
+    }
+    return;
+}
 // Driver's code
 int main()
 {
-    CreateLinkedList(1);printf("%d %d %d\n", head->x, head->next->x, tail->x);
-    insert_tail(2);printf("%d %d %d\n", head->x, head->next->x, tail->x);
-    insert_head(3);printf("%d %d %d\n", head->x, head->next->x, tail->x);
-    insert_tail(4);printf("%d %d %d %d\n", head->x, head->next->x, head->next->next->x,tail->x);
-    insert_head(5);printf("%d %d %d %d\n", head->x, head->next->x, head->next->next->x,head->next->next->next->x,tail->x);
+    CreateLinkedList(1);data_count++;
+    insert_tail(2);data_count++;
+    insert_head(3);data_count++;
+    insert_tail(4);data_count++;
+    insert_head(5);data_count++;
+    printf("%d %d %d %d %d\n", head->x, head->next->x, head->next->next->x,head->next->next->next->x,tail->x);
+    delete_element(4);
+    printf("%d %d %d %d\n", head->x, head->next->x, head->next->next->x,tail->x);
     return 0;
 }
